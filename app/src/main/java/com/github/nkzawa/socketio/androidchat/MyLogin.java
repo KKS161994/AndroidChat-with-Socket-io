6b7cd390d3c7f076261ba2964931a6ce5b2958f6
KKS161994 <kartikeyshrivastava@gmail.com>
2016-06-04 16:44:19 +0530

    Chat for group completed

---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/MyLogin.java
@@ -0,0 +1,71 @@
+package com.github.nkzawa.socketio.androidchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by shashankgupta on 04/06/16.
+ */
+public class MyLogin extends Activity {
+
+    private EditText mUsernameView;
+
+    private String mUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUsernameView = (EditText) findViewById(R.id.username_input);
+        mUsernameView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    private void attemptLogin() {
+        // Reset errors.
+        mUsernameView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString().trim();
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            mUsernameView.setError(getString(R.string.error_field_required));
+            mUsernameView.requestFocus();
+            return;
+        }
+
+        mUsername = username;
+        Intent intent=new Intent(MyLogin.this,MainActivity.class);
+        intent.putExtra("username", mUsername);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/github/nkzawa/socketio/androidchat/MySocket.java
@@ -0,0 +1,196 @@
+package com.github.nkzawa.socketio.androidchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+/**
+ * Created by shashankgupta on 28/05/16.
+ */
+public class MySocket extends Fragment {
+
+    private List<Message> mMessages = new ArrayList<Message>();
+    private RecyclerView.Adapter mAdapter;
+    private EditText mInputMessageView;
+    private RecyclerView mMessagesView;
+    private Socket mSocket;
+    private ImageButton btnSend;
+    String username="";
+    {
+        try {
+            mSocket = IO.socket(Constants.CHAT_SERVER_URL);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public MySocket() {
+        super();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mAdapter = new MessageAdapter(activity, mMessages);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+        Bundle extra=getActivity().getIntent().getExtras();
+        if(extra!=null){
+            username=extra.getString("username");
+        }
+
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        mSocket.on("message", onNewMessage);
+        //mSocket.on("user joined", onUserJoined);
+        //mSocket.on("user left", onUserLeft);
+        //mSocket.on("typing", onTyping);
+        //mSocket.on("stop typing", onStopTyping);
+        mSocket.connect();
+
+        //startSignIn();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMessagesView = (RecyclerView) view.findViewById(R.id.messages);
+        mMessagesView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mMessagesView.setAdapter(mAdapter);
+
+        mInputMessageView = (EditText) view.findViewById(R.id.message_input);
+        mInputMessageView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int id, KeyEvent event) {
+                if (id == R.id.send || id == EditorInfo.IME_NULL) {
+                    attemptSend();
+                    return true;
+                }
+                return false;
+            }
+        });
+        btnSend=(ImageButton)view.findViewById(R.id.send_button);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptSend();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+        mSocket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.off(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        mSocket.off("new message", onNewMessage);
+        //mSocket.off("user joined", onUserJoined);
+        //mSocket.off("user left", onUserLeft);
+        //mSocket.off("typing", onTyping);
+        //mSocket.off("stop typing", onStopTyping);
+    }
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            R.string.error_connect, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //JSONObject data = (JSONObject) args[0];
+                    String username=(String) args[0];
+                    String message= (String) args[1];
+
+
+                    addMessage(username, message);
+                }
+            });
+        }
+    };
+
+    private void addMessage(String username, String message) {
+        mMessages.add(new Message.Builder(Message.TYPE_MESSAGE)
+                .username(username).message(message).build());
+        mAdapter.notifyItemInserted(mMessages.size() - 1);
+        scrollToBottom();
+    }
+
+    private void attemptSend() {
+        //if (null == mUsername) return;
+        if (!mSocket.connected()) {
+            Toast.makeText(getActivity(), "Socket not connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //mTyping = false;
+
+        String message = mInputMessageView.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            mInputMessageView.requestFocus();
+            return;
+        }
+
+        mInputMessageView.setText("");
+        addMessage(username, message);
+
+        // perform the sending message attempt.
+        mSocket.emit("chat", username,message);
+    }
+
+    private void scrollToBottom() {
+        mMessagesView.scrollToPosition(mAdapter.getItemCount() - 1);
+    }
+
+
+}
